83cf34386da64789853b4fd362ea26116ef71fc1
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-08-20 18:11:31 +0600

    completes

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -2,12 +2,29 @@
 import {ref} from "vue";
 import ChildComponent from "./ChildComponent.vue";
 
-const message = ref("Hello World")
+const message = ref("Hello World !! I am from parent components")
+
+const messageReceivedFromChild = ref('')
+
+function myAction(n) {
+  messageReceivedFromChild.value = n
+}
 
 </script>
 
 <template>
-  <ChildComponent v-model:message="message"/>
+
+  <ChildComponent @message-Emitted="myAction" v-model:message="message"/>
+
+  <div v-show="messageReceivedFromChild">
+    <h1 class="text-black-900 text-xl my-5"><span class="text-green-500">
+     Parent Component Received the message From Child Component through Emit :
+       <span class="text-black font-bold">{{ messageReceivedFromChild }}
+       </span>
+     </span></h1>
+
+  </div>
+
 </template>
 
 <style scoped>
