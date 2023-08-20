fab390aaddc039e990011837c61caf0b27d768c6
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-08-20 14:46:24 +0600

    complete

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps(['message'])
+const messageEmitted = defineEmits(['update:message'])
+
+function showMessage() {
+  props.name = messageEmitted('update:message', "This is the updated message clicked by the child component button")
+}
+</script>
+
+<template>
+  <h1 class="my-5">Props Received and Show the message:</h1>
+  <br>
+  <h2 class="text-black-900 text-xl my-5"><span class="text-red-600">{{ message }}</span></h2>
+  <br>
+  <br>
+  <button @click="showMessage()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click Me
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+const message = ref("Hello World")
+
+</script>
+
+<template>
+  <ChildComponent v-model:message="message"/>
+</template>
+
+<style scoped>
+
+</style>
